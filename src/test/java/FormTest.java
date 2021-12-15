ac4adbb75520e42afc57e71569eb08f68df7b30e
DD <deema777xxx@gmail.com>
2021-12-15 12:14:13 +0300

    form test

--- src/test/java/FormTest.java
+++ src/test/java/FormTest.java
@@ -1,2 +1,35 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class FormTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.startMaximized = true;
+
+    }
+
+    @Test
+    void fillFormTest() {
+        open("https://demoqa.com/text-box");
+        $("[id=userName]").setValue("Dimitry777");
+        $("#userEmail").setValue("Dimitry777@mail.ru");
+        $("#currentAddress").setValue("Moscow");
+        $("#permanentAddress").setValue("Piter");
+        $("#submit").scrollTo();
+        $("#submit").click();
+
+        $("#output #name").shouldHave(text("Dimitry777"));
+        $("#output").$("#email").shouldHave(text("Dimitry777"));
+        $("#output").$("#currentAddress").shouldHave(text("Moscow"));
+        $("#output").$("#permanentAddress").shouldHave(text("Piter"));
+
+//        $("#output").shouldHave(text("Some name"), text("aaa@aa.aa"),
+//                text("Some address"), text("Another address"));
+    }
 }
